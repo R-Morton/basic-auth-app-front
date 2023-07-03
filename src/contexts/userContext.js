95906d25292dd1beb44d657748b73aef94d9a008
R-Morton <remann12@gmail.com>
2023-07-03 19:47:12 +0100

    basic setup with user reducer

---
+++ src/contexts/userContext.js
@@ -0,0 +1,48 @@
+import { createContext, useContext, useReducer, useEffect } from "react"
+import { useLocalStorage } from "react-use"
+import { initialMedicalData } from "../data/InitialClientData"
+import { userReducer } from "../reducers/userReducer"
+
+let defaultUserValues = {
+    userId: '',
+    displayName: ''
+}
+
+export const UserDataContext = createContext(null)
+export const UserDispatchContext = createContext(null)
+
+// Custom hook for read only data
+export function useUserData() {
+    return useContext(UserDataContext)
+}
+
+// Custom hook for write/dispatch data
+export function useUserDispatch() {
+    return useContext(UserDispatchContext)
+}
+
+export default function UserProvider(props) {
+
+    const [userData, userDispatch] = useReducer(userReducer, defaultUserValues)
+
+    const [persistantData, setPersistantData] = useLocalStorage('user', defaultUserValues)
+
+    useEffect(() => {
+        medicalDispatch({type:"setup", data: persistantData})
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    //Autosaves changes to noted from reducer state into localstorage
+    useEffect(() => {
+        setPersistantData(userData)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [userData])
+
+    return(
+        <UserDataContext.Provider value={userData}>
+            <UserDispatchContext.Provider value={userDispatch}>
+                {props.children}
+            </UserDispatchContext.Provider>
+        </UserDataContext.Provider>
+    )
+}
